2ca9e13075d59f01bde1317b8a1efcfa6793efd9
Camille <>
2021-04-16 13:17:35 +0200

    data validation

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -10,10 +10,15 @@ namespace BugTrackerUI
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(10, ErrorMessage="The minimum length for description is 10 caracters.")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(1,5, ErrorMessage="The priorit must be between 1 and 5.")]
         public int Priority { get; set; }
     }
 }
